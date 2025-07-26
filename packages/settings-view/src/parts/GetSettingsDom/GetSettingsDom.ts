a2c57f42e245c8d348eddaa6a5f2e88624623659
Le Vivilet <levivlet@pm.me>
2025-07-26 23:44:32 +0200

    feature: update settings dom

--- packages/settings-view/src/parts/GetSettingsDom/GetSettingsDom.ts
+++ packages/settings-view/src/parts/GetSettingsDom/GetSettingsDom.ts
@@ -1,12 +1,14 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getSettingsDom = (state: SettingsState): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
       childCount: 1,
+      className: mergeClassNames(ClassNames.Viewlet, ClassNames.Settings),
     },
     {
       type: VirtualDomElements.H1,

f39a8f2cf60cc285007cd13b5d76ffadc58ed32b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-26 23:46:00 +0200

    feature: add saveState (#21)

--- packages/settings-view/src/parts/CommandMap/CommandMap.ts
+++ packages/settings-view/src/parts/CommandMap/CommandMap.ts
@@ -6,16 +6,20 @@ import * as LoadContent from '../LoadContent/LoadContent.ts'
 import { render2 } from '../Render2/Render2.ts'
 import { renderActions } from '../RenderActions/RenderActions.ts'
 import { renderEventListeners } from '../RenderEventListeners/RenderEventListeners.ts'
+import { restoreState } from '../RestoreState/RestoreState.ts'
+import { saveState } from '../SaveState/SaveState.ts'
 import { getCommandIds, wrapCommand } from '../SettingsStates/SettingsStates.ts'
 
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'Settings.create': Create.create,
-  'Settings.getCommandIds': getCommandIds,
-  'Settings.terminate': terminate,
   'Settings.diff2': Diff2.diff2,
+  'Settings.getCommandIds': getCommandIds,
   'Settings.loadContent': wrapCommand(LoadContent.loadContent),
-  'Settings.renderActions': renderActions,
   'Settings.render2': render2,
+  'Settings.renderActions': renderActions,
   'Settings.renderEventListeners': renderEventListeners,
+  'Settings.restoreState': restoreState,
+  'Settings.saveState': saveState,
+  'Settings.terminate': terminate,
 }

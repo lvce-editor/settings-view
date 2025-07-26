9e25015672d45902e1b86c75ad91bed2a65191aa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-26 23:35:35 +0200

    feature: add diffitems (#15)

--- packages/settings-view/src/parts/DiffModules/DiffModules.ts
+++ packages/settings-view/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,7 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 
-export const modules = [DiffFocus.isEqual]
+export const modules = [DiffFocus.isEqual, DiffItems.isEqual]
 
-export const numbers = [DiffType.RenderFocus]
+export const numbers = [DiffType.RenderFocus, DiffType.RenderItems]

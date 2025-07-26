d4a9ad27736f91f5f9c4d6b64eb23c6216098e5e
Le Vivilet <levivlet@pm.me>
2025-07-26 23:36:49 +0200

    feature: register commands

--- packages/settings-view/src/parts/SettingsStates/SettingsStates.ts
+++ packages/settings-view/src/parts/SettingsStates/SettingsStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
-export const { get, set, wrapCommand, dispose, getKeys, getCommandIds } = ViewletRegistry.create<SettingsState>()
+export const { get, set, wrapCommand, dispose, getKeys, getCommandIds, registerCommands } = ViewletRegistry.create<SettingsState>()

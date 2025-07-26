8a9fa6fe29a801aea9427271fb25e93364f448e5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-26 23:18:00 +0200

    feature: add terminate command (#4)

--- packages/settings-view/src/parts/CommandMap/CommandMap.ts
+++ packages/settings-view/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
+import { terminate } from '@lvce-editor/viewlet-registry'
 import * as Initialize from '../Initialize/Initialize.ts'
 
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
+  'Settings.terminate': terminate,
 }

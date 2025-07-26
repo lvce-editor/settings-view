fede40b33d51771732b33a01627e2476b1ff37d2
Le Vivilet <levivlet@pm.me>
2025-07-26 23:30:21 +0200

    feature: add command ids

--- packages/settings-view/src/parts/CommandMap/CommandMap.ts
+++ packages/settings-view/src/parts/CommandMap/CommandMap.ts
@@ -3,11 +3,12 @@ import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
-import { wrapCommand } from '../SettingsStates/SettingsStates.ts'
+import { getCommandIds, wrapCommand } from '../SettingsStates/SettingsStates.ts'
 
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'Settings.create': Create.create,
+  'Settings.getCommandIds': getCommandIds,
   'Settings.terminate': terminate,
   'Settings.diff2': Diff2.diff2,
   'Settings.loadContent': wrapCommand(LoadContent.loadContent),

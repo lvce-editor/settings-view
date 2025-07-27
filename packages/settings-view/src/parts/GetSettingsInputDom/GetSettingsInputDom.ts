5f3e940bf212c8383f89a65020604ff8d6409014
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 11:16:56 +0200

    feature: add input classname (#49)

--- packages/settings-view/src/parts/GetSettingsInputDom/GetSettingsInputDom.ts
+++ packages/settings-view/src/parts/GetSettingsInputDom/GetSettingsInputDom.ts
@@ -11,7 +11,7 @@ export const getSettingsInputDom = (): readonly VirtualDomNode[] => {
     },
     {
       type: VirtualDomElements.Input,
-      className: 'SettingsSearchInput',
+      className: 'InputBox SettingsSearchInput',
       placeholder,
     },
   ]

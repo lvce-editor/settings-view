ef7c2a3e3a9e80818956d417ac5e1cdd2139558b
Le Vivilet <levivlet@pm.me>
2025-07-27 11:14:13 +0200

    feature: move tabs into separate file

---
+++ packages/settings-view/src/parts/GetSettingsTabsDom/GetSettingsTabsDom.ts
@@ -0,0 +1,25 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+const getTabVirtualDom = (tab: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Tab',
+      childCount: 1,
+    },
+    text(tab),
+  ]
+}
+
+export const getSettingsTabsDom = (tabs: readonly string[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Ul,
+      className: 'SettingsTabs',
+      role: AriaRoles.TabList,
+      childCount: tabs.length,
+    },
+    ...tabs.flatMap(getTabVirtualDom),
+  ]
+}

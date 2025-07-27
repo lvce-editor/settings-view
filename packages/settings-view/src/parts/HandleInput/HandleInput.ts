1962f43a6a950dbb7d9464e8451ec7168f7cf44f
Le Vivilet <levivlet@pm.me>
2025-07-27 22:08:29 +0200

    feature: set input source in handleInput

--- packages/settings-view/src/parts/HandleInput/HandleInput.ts
+++ packages/settings-view/src/parts/HandleInput/HandleInput.ts
@@ -1,5 +1,6 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
+import { User } from '../InputSource/InputSource.ts'
 
 export const handleInput = (state: SettingsState, value: string): SettingsState => {
   const { items, tabs } = state
@@ -8,5 +9,6 @@ export const handleInput = (state: SettingsState, value: string): SettingsState 
     ...state,
     searchValue: value,
     filteredItems,
+    inputSource: User,
   }
 }

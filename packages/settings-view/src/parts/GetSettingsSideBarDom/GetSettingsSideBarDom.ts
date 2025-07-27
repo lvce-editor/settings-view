a20b8fc4b31bef3d11bc87a387ea16b7c6f57c96
Le Vivilet <levivlet@pm.me>
2025-07-27 10:42:44 +0200

    feature: add sidebar dom

---
+++ packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
@@ -0,0 +1,24 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+const getTabVirtualDom = (tab: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Tab',
+      childCount: 1,
+    },
+    text(tab),
+  ]
+}
+
+export const getSettingsSideBarDom = (tabs: readonly string[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Aside,
+      className: 'SettingsSideBar',
+      childCount: tabs.length,
+    },
+    ...tabs.flatMap(getTabVirtualDom),
+  ]
+}

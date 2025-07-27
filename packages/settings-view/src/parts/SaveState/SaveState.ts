754520e2a8cfc56dabc98d4c651156eec32a728d
Le Vivilet <levivlet@pm.me>
2025-07-27 17:34:54 +0200

    fix

--- packages/settings-view/src/parts/SaveState/SaveState.ts
+++ packages/settings-view/src/parts/SaveState/SaveState.ts
@@ -1,9 +1,9 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
+import { SettingsState } from '../SettingsState/SettingsState.ts'
 import { get } from '../SettingsStates/SettingsStates.ts'
 
-export const saveState = (uid: number): SavedState => {
-  const { newState } = get(uid)
-  const selectedTab = newState.tabs.find((tab) => tab.selected)?.id || ''
+export const saveState = (state: SettingsState): SavedState => {
+  const selectedTab = state.tabs.find((tab) => tab.selected)?.id || ''
 
   return {
     expandedPaths: [],
@@ -11,7 +11,7 @@ export const saveState = (uid: number): SavedState => {
     minLineY: 0,
     maxLineY: 0,
     deltaY: 0,
-    searchValue: newState.searchValue,
+    searchValue: state.searchValue,
     selectedTab,
   }
 }

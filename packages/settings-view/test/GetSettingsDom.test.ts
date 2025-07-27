5a54f40df21d3adfc9e111636e8ee93eadf24622
Le Vivilet <levivlet@pm.me>
2025-07-27 17:43:03 +0200

    feature: update heading depending on which tab is selected

--- packages/settings-view/test/GetSettingsDom.test.ts
+++ packages/settings-view/test/GetSettingsDom.test.ts
@@ -2,9 +2,14 @@ import { expect, test } from '@jest/globals'
 import type { SettingsState } from '../src/parts/SettingsState/SettingsState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { getSettingsDom } from '../src/parts/GetSettingsDom/GetSettingsDom.ts'
+import { getTabs } from '../src/parts/GetTabs/GetTabs.ts'
 
 test('getSettingsDom returns array of VirtualDomNode objects', () => {
-  const mockState: SettingsState = createDefaultState()
+  const mockState: SettingsState = {
+    ...createDefaultState(),
+    tabs: getTabs(),
+    filteredItems: [],
+  }
   const domNodes = getSettingsDom(mockState)
   expect(domNodes).toBeDefined()
 })

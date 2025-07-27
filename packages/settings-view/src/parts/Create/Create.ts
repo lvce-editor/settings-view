dbe68f65230cdd6d4874f5a2aaca27d3b7a7580d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 12:51:30 +0200

    feature: add items property (#61)

--- packages/settings-view/src/parts/Create/Create.ts
+++ packages/settings-view/src/parts/Create/Create.ts
@@ -13,6 +13,7 @@ export const create = (id: number, uri: string, x: number, y: number, width: num
     width,
     height,
     tabs: [],
+    items: [],
   }
   SettingsStates.set(id, state, state)
 }

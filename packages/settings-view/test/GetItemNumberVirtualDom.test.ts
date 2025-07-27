876ab819471123fffc25408157bc7b806183cf99
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 14:50:43 +0200

    feature: add number dom test (#86)

---
+++ packages/settings-view/test/GetItemNumberVirtualDom.test.ts
@@ -0,0 +1,244 @@
+import { expect, test } from '@jest/globals'
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { getItemNumberVirtualDom } from '../src/parts/GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts'
+
+test('getItemNumberVirtualDom returns expected DOM structure for normal item', () => {
+  const item = {
+    heading: 'Test Heading',
+    description: 'Test Description',
+    type: 1,
+    value: '42',
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text('Test Heading'),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text('Test Description'),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})
+
+test('getItemNumberVirtualDom handles empty strings', () => {
+  const item = {
+    heading: '',
+    description: '',
+    type: 1,
+    value: '',
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text(''),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text(''),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})
+
+test('getItemNumberVirtualDom handles special characters in heading and description', () => {
+  const item = {
+    heading: 'Heading with & < > " \' chars',
+    description: 'Description with & < > " \' chars',
+    type: 1,
+    value: '123',
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text('Heading with & < > " \' chars'),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text('Description with & < > " \' chars'),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})
+
+test('getItemNumberVirtualDom handles long text', () => {
+  const item = {
+    heading: 'This is a very long heading that might wrap to multiple lines in the UI',
+    description: 'This is a very long description that contains a lot of text and might also wrap to multiple lines in the user interface',
+    type: 1,
+    value: '999999',
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text('This is a very long heading that might wrap to multiple lines in the UI'),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text('This is a very long description that contains a lot of text and might also wrap to multiple lines in the user interface'),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})
+
+test('getItemNumberVirtualDom handles numeric values in item', () => {
+  const item = {
+    heading: 'Numeric Test',
+    description: 'Testing with numeric values',
+    type: 42,
+    value: '0',
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text('Numeric Test'),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text('Testing with numeric values'),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})
+
+test('getItemNumberVirtualDom maintains consistent structure regardless of content', () => {
+  const items = [
+    {
+      heading: 'Item 1',
+      description: 'Description 1',
+      type: 1,
+      value: '1',
+    },
+    {
+      heading: 'Item 2',
+      description: 'Description 2',
+      type: 2,
+      value: '2',
+    },
+    {
+      heading: 'Item 3',
+      description: 'Description 3',
+      type: 3,
+      value: '3',
+    },
+  ]
+
+  for (const item of items) {
+    const virtualDom = getItemNumberVirtualDom(item)
+
+    expect(virtualDom).toHaveLength(6)
+    expect(virtualDom[0]).toEqual({
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    })
+    expect(virtualDom[1]).toEqual({
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    })
+    expect(virtualDom[2]).toEqual(text(item.heading))
+    expect(virtualDom[3]).toEqual({
+      type: VirtualDomElements.P,
+      childCount: 1,
+    })
+    expect(virtualDom[4]).toEqual(text(item.description))
+    expect(virtualDom[5]).toEqual({
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: 'number value',
+      childCount: 0,
+    })
+  }
+})

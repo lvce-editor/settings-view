f292161a8ff33fe03f2888baaa5bd877fb683d5f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 13:07:07 +0200

    feature: make tab an interface with selected and label property (#65)
    * feature: make tab an interface with selected and label property
    
    * format

--- packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
+++ packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
@@ -1,8 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { Tab } from '../Tab/Tab.ts'
 import { getSettingsTabsDom } from '../GetSettingsTabsDom/GetSettingsTabsDom.ts'
 
-export const getSettingsSideBarDom = (tabs: readonly string[]): readonly VirtualDomNode[] => {
+export const getSettingsSideBarDom = (tabs: readonly Tab[]): readonly VirtualDomNode[] => {
   const sideBarHeading = 'Settings SideBar'
   return [
     {

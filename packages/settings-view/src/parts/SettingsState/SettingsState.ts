80b77ff5f5f014a42ea9e60066de4355278d26bc
Le Vivilet <levivlet@pm.me>
2025-07-27 12:50:46 +0200

    feature: add items property

--- packages/settings-view/src/parts/SettingsState/SettingsState.ts
+++ packages/settings-view/src/parts/SettingsState/SettingsState.ts
@@ -1,9 +1,12 @@
+import type { SettingItem } from '../SettingItem/SettingItem.ts'
+
 export interface SettingsState {
   readonly breakPointsExpanded: boolean
   readonly breakPointsVisible: boolean
   readonly focus: number
   readonly height: number
   readonly id: number
+  readonly items: readonly SettingItem[]
   readonly tabs: readonly string[]
   readonly uri: string
   readonly width: number

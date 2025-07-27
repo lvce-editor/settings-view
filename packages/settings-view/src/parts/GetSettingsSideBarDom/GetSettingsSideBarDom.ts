b2b058fd06632cff8b717dfd31c23c3a8baaa9d9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 10:54:17 +0200

    feature: format code (#32)

--- packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
+++ packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
@@ -1,4 +1,4 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
 const getTabVirtualDom = (tab: string): readonly VirtualDomNode[] => {

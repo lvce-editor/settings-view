2dae620497334cd67de9dcac8b0b5c7bb64fb17a
Le Vivilet <levivlet@pm.me>
2025-07-27 17:57:45 +0200

    structure

--- packages/settings-view/src/parts/SaveState/SaveState.ts
+++ packages/settings-view/src/parts/SaveState/SaveState.ts
@@ -2,15 +2,15 @@ import type { SavedState } from '../SavedState/SavedState.ts'
 import { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const saveState = (state: SettingsState): SavedState => {
-  const selectedTab = state.tabs.find((tab) => tab.selected)?.id || ''
-
+  const { tabs, searchValue } = state
+  const selectedTab = tabs.find((tab) => tab.selected)?.id || ''
   return {
     expandedPaths: [],
     root: '',
     minLineY: 0,
     maxLineY: 0,
     deltaY: 0,
-    searchValue: state.searchValue,
+    searchValue,
     selectedTab,
   }
 }

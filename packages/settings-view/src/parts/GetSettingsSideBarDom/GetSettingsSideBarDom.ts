896d4835da5203aad162bb48b95e50c0c232410b
Le Vivilet <levivlet@pm.me>
2025-07-27 22:07:27 +0200

    feature: remove unused sidebar heading

--- packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
+++ packages/settings-view/src/parts/GetSettingsSideBarDom/GetSettingsSideBarDom.ts
@@ -1,23 +1,15 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Tab } from '../Tab/Tab.ts'
 import { getSettingsTabsDom } from '../GetSettingsTabsDom/GetSettingsTabsDom.ts'
-import * as SettingStrings from '../SettingStrings/SettingStrings.ts'
 
 export const getSettingsSideBarDom = (tabs: readonly Tab[]): readonly VirtualDomNode[] => {
-  const sideBarHeading = SettingStrings.settingsSideBar()
   return [
     {
       type: VirtualDomElements.Aside,
       className: 'SettingsSideBar',
-      childCount: 2,
-    },
-    {
-      type: VirtualDomElements.H2,
-      className: 'SettingsSideBarHeading',
       childCount: 1,
     },
-    text(sideBarHeading),
     ...getSettingsTabsDom(tabs),
   ]
 }

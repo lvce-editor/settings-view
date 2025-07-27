9fb448d02ce1161bee969288ea06fac9f8465222
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 17:43:51 +0200

    feature: update heading depending on which tab is selected (#102)

--- packages/settings-view/src/parts/GetSettingsContentDom/GetSettingsContentDom.ts
+++ packages/settings-view/src/parts/GetSettingsContentDom/GetSettingsContentDom.ts
@@ -1,9 +1,13 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
+import type { Tab } from '../Tab/Tab.ts'
 import { getSettingsItemsDom } from '../GetSettingsItemsDom/GetSettingsItemsDom.ts'
 
-export const getSettingsContentDom = (items: readonly SettingItem[]): readonly VirtualDomNode[] => {
+export const getSettingsContentDom = (items: readonly SettingItem[], tabs: readonly Tab[]): readonly VirtualDomNode[] => {
+  const selectedTab = tabs.find((tab) => tab.selected)
+  const headerText = selectedTab ? selectedTab.label : 'Settings Content'
+
   return [
     {
       type: VirtualDomElements.Div,
@@ -14,7 +18,7 @@ export const getSettingsContentDom = (items: readonly SettingItem[]): readonly V
       type: VirtualDomElements.H1,
       childCount: 1,
     },
-    text('Settings Content'),
+    text(headerText),
     ...getSettingsItemsDom(items),
   ]
 }

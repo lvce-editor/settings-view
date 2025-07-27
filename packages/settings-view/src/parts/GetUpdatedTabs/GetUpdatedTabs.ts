54d56b8b1893c3e81802bca6fa3fb4046fa8443f
Le Vivilet <levivlet@pm.me>
2025-07-27 14:53:53 +0200

    better update

--- packages/settings-view/src/parts/GetUpdatedTabs/GetUpdatedTabs.ts
+++ packages/settings-view/src/parts/GetUpdatedTabs/GetUpdatedTabs.ts
@@ -1,7 +1,13 @@
 import type { Tab } from '../Tab/Tab.ts'
 
 export const getUpdatedTabs = (tabs: readonly Tab[], selectedTabName: string): readonly Tab[] => {
-  return tabs.map((tab) => ({
+  // Check if the clicked tab is already selected
+  const clickedTab = tabs.find(tab => tab.label === selectedTabName)
+  if (clickedTab?.selected) {
+    return tabs
+  }
+
+  return tabs.map(tab => ({
     ...tab,
     selected: tab.label === selectedTabName,
   }))

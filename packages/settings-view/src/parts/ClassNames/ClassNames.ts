c54efac8922ac114a16ef4b375391878437d6bce
Le Vivilet <levivlet@pm.me>
2025-07-27 18:58:04 +0200

    feature: remove unused classnames

--- packages/settings-view/src/parts/ClassNames/ClassNames.ts
+++ packages/settings-view/src/parts/ClassNames/ClassNames.ts
@@ -1,37 +1,37 @@
 import { ClassNames } from '@lvce-editor/virtual-dom-worker'
 
 export const {
-  CallStackArrow,
-  CallStackDescription,
-  CallStackLabel,
-  Chevron,
-  DebugButton,
-  DebugButtons,
-  DebugPausedMessage,
-  DebugPropertyChevron,
+  
+  
+  
+  
+  
+  
+  
+  
   Viewlet,
-  DebugPropertyKey,
-  DebugRow,
-  DebugRowCallStack,
-  DebugRowCheckBox,
-  DebugRowInputField,
-  DebugSectionAction,
-  DebugSectionActions,
-  DebugSectionHeader,
-  DebugValue,
-  DebugValueBoolean,
-  DebugValueFunction,
-  DebugValueGetter,
-  DebugValueNumber,
-  DebugValueObject,
-  DebugValueScopeName,
-  DebugValueString,
-  DebugValueSymbol,
-  DebugValueUndefined,
-  DeleteWatchExpression,
-  IconButton,
-  InputBox,
-  InputLabel,
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 } = ClassNames
 
 export const Settings = 'Settings'

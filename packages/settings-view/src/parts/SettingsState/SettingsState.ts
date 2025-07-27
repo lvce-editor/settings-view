36165b5feb5a5fab4c1a3643a6dfed8dad62b728
Le Vivilet <levivlet@pm.me>
2025-07-27 12:46:06 +0200

    feature: sort state

--- packages/settings-view/src/parts/SettingsState/SettingsState.ts
+++ packages/settings-view/src/parts/SettingsState/SettingsState.ts
@@ -2,11 +2,11 @@ export interface SettingsState {
   readonly breakPointsExpanded: boolean
   readonly breakPointsVisible: boolean
   readonly focus: number
+  readonly height: number
   readonly id: number
+  readonly tabs: readonly string[]
   readonly uri: string
+  readonly width: number
   readonly x: number
   readonly y: number
-  readonly width: number
-  readonly height: number
-  readonly tabs: readonly string[]
 }

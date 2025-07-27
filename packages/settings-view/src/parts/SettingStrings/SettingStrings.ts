2f91f13e87b3840de331191884e6f8c3f67f404b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 18:15:00 +0200

    feature: add more settings strings (#107)

--- packages/settings-view/src/parts/SettingStrings/SettingStrings.ts
+++ packages/settings-view/src/parts/SettingStrings/SettingStrings.ts
@@ -1,12 +1,84 @@
 import * as I18NString from '../I18NString/I18NString.ts'
 import * as UiStrings from '../UiStrings/UiStrings.ts'
 
+export const addWatchExpression = (): string => {
+  return I18NString.i18nString(UiStrings.AddWatchExpression)
+}
+
+export const autoSave = (): string => {
+  return I18NString.i18nString(UiStrings.AutoSave)
+}
+
+export const autoSaveDescription = (): string => {
+  return I18NString.i18nString(UiStrings.AutoSaveDescription)
+}
+
+export const autoUpdateExtensions = (): string => {
+  return I18NString.i18nString(UiStrings.AutoUpdateExtensions)
+}
+
+export const autoUpdateExtensionsDescription = (): string => {
+  return I18NString.i18nString(UiStrings.AutoUpdateExtensionsDescription)
+}
+
+export const autoUpdates = (): string => {
+  return I18NString.i18nString(UiStrings.AutoUpdates)
+}
+
+export const autoUpdatesDescription = (): string => {
+  return I18NString.i18nString(UiStrings.AutoUpdatesDescription)
+}
+
 export const clear = (): string => {
   return I18NString.i18nString(UiStrings.Clear)
 }
 
+export const extensionRecommendations = (): string => {
+  return I18NString.i18nString(UiStrings.ExtensionRecommendations)
+}
+
+export const extensionRecommendationsDescription = (): string => {
+  return I18NString.i18nString(UiStrings.ExtensionRecommendationsDescription)
+}
+
+export const fileEncryption = (): string => {
+  return I18NString.i18nString(UiStrings.FileEncryption)
+}
+
+export const fileEncryptionDescription = (): string => {
+  return I18NString.i18nString(UiStrings.FileEncryptionDescription)
+}
+
+export const fontFamily = (): string => {
+  return I18NString.i18nString(UiStrings.FontFamily)
+}
+
+export const fontFamilyDescription = (): string => {
+  return I18NString.i18nString(UiStrings.FontFamilyDescription)
+}
+
+export const fontSize = (): string => {
+  return I18NString.i18nString(UiStrings.FontSize)
+}
+
+export const fontSizeDescription = (): string => {
+  return I18NString.i18nString(UiStrings.FontSizeDescription)
+}
+
+export const formatOnSave = (): string => {
+  return I18NString.i18nString(UiStrings.FormatOnSave)
+}
+
+export const formatOnSaveDescription = (): string => {
+  return I18NString.i18nString(UiStrings.FormatOnSaveDescription)
+}
+
 export const numberValue = (): string => {
   return I18NString.i18nString(UiStrings.NumberValue)
+}
+
+export const refreshWatchExpressions = (): string => {
+  return I18NString.i18nString(UiStrings.RefreshWatchExpressions)
 }
 
 export const searchSettings = (): string => {
@@ -19,4 +91,52 @@ export const settingsContent = (): string => {
 
 export const settingsSideBar = (): string => {
   return I18NString.i18nString(UiStrings.SettingsSideBar)
+}
+
+export const sidebarPosition = (): string => {
+  return I18NString.i18nString(UiStrings.SidebarPosition)
+}
+
+export const sidebarPositionDescription = (): string => {
+  return I18NString.i18nString(UiStrings.SidebarPositionDescription)
+}
+
+export const telemetry = (): string => {
+  return I18NString.i18nString(UiStrings.Telemetry)
+}
+
+export const telemetryDescription = (): string => {
+  return I18NString.i18nString(UiStrings.TelemetryDescription)
+}
+
+export const theme = (): string => {
+  return I18NString.i18nString(UiStrings.Theme)
+}
+
+export const themeDescription = (): string => {
+  return I18NString.i18nString(UiStrings.ThemeDescription)
+}
+
+export const twoFactorAuth = (): string => {
+  return I18NString.i18nString(UiStrings.TwoFactorAuth)
+}
+
+export const twoFactorAuthDescription = (): string => {
+  return I18NString.i18nString(UiStrings.TwoFactorAuthDescription)
+}
+
+export const windowSize = (): string => {
+  return I18NString.i18nString(UiStrings.WindowSize)
+}
+
+export const windowSizeDescription = (): string => {
+  return I18NString.i18nString(UiStrings.WindowSizeDescription)
+}
+
+export const windowTitle = (): string => {
+  return I18NString.i18nString(UiStrings.WindowTitle)
+}
+
+export const windowTitleDescription = (): string => {
+  return I18NString.i18nString(UiStrings.WindowTitleDescription)
 }

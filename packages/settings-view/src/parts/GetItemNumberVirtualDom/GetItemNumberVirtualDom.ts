1bbba47269328bdfc9b23d55658d7f65260146d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 12:53:08 +0200

    feature: add number dom (#63)

---
+++ packages/settings-view/src/parts/GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts
@@ -0,0 +1,28 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { SettingItem } from '../SettingItem/SettingItem.ts'
+
+export const getItemNumberVirtualDom = (item: SettingItem): readonly VirtualDomNode[] => {
+  const { heading, description } = item
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text(heading),
+    {
+      type: VirtualDomElements.P,
+      childCount: 1,
+    },
+    text(description),
+    {
+      type: VirtualDomElements.Input,
+      childCount: 0,
+    },
+  ]
+}

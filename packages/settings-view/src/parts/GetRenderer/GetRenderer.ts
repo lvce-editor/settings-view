6406fcdb7a696043171bf94f68ac303f5d11da8e
Le Vivilet <levivlet@pm.me>
2025-07-27 22:10:47 +0200

    feature: add renderValue

--- packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
@@ -2,11 +2,14 @@ import type { Renderer } from '../Renderer/Renderer.ts'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
+import { renderValue } from '../RenderValue/RenderValue.ts'
 
 export const getRenderer = (diffType: number): Renderer<SettingsState> => {
   switch (diffType) {
     case DiffType.RenderItems:
       return RenderItems.renderItems
+    case DiffType.RenderValue:
+      return renderValue
     default:
       throw new Error('unknown renderer')
   }

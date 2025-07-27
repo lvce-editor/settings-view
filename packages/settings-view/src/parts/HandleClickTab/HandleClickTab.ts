7cd1dab0d813af1d85a058d3c5bb1029c4577dc5
Le Vivilet <levivlet@pm.me>
2025-07-27 14:38:37 +0200

    feature: dont modify state if selected tab is invalid

--- packages/settings-view/src/parts/HandleClickTab/HandleClickTab.ts
+++ packages/settings-view/src/parts/HandleClickTab/HandleClickTab.ts
@@ -1,6 +1,9 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const handleClickTab = (state: SettingsState, name: string): SettingsState => {
+  if (!name) {
+    return state
+  }
   // TODO mark that tab as selected
   return {
     ...state,

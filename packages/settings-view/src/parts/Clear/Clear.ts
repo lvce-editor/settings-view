6973e5fd21482399978c5f05c793c5f4a7ff2c5f
Le Vivilet <levivlet@pm.me>
2025-07-27 22:20:38 +0200

    feature: mark input source as script when clicking on clear

--- packages/settings-view/src/parts/Clear/Clear.ts
+++ packages/settings-view/src/parts/Clear/Clear.ts
@@ -1,8 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { Script } from '../InputSource/InputSource.ts'
 
 export const clear = (state: SettingsState): SettingsState => {
   return {
     ...state,
     searchValue: '',
+    inputSource: Script,
   }
 }

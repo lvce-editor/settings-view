fa5e2b710e0c3b2a9cbf77ae6eb925504c8585f1
Le Vivilet <levivlet@pm.me>
2025-07-27 22:15:30 +0200

    feature: diff value

--- packages/settings-view/src/parts/DiffModules/DiffModules.ts
+++ packages/settings-view/src/parts/DiffModules/DiffModules.ts
@@ -1,7 +1,8 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffFocus.isEqual, DiffItems.isEqual]
+export const modules = [DiffFocus.isEqual, DiffItems.isEqual, DiffValue.isEqual]
 
-export const numbers = [DiffType.RenderFocus, DiffType.RenderItems]
+export const numbers = [DiffType.RenderFocus, DiffType.RenderItems, DiffType.RenderValue]

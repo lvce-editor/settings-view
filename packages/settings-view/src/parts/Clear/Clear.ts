9f9bf02fd783cffc097d559a9a40f7b40761f7f0
Le Vivilet <levivlet@pm.me>
2025-07-27 22:41:32 +0200

    feature: update filtered items in clear

--- packages/settings-view/src/parts/Clear/Clear.ts
+++ packages/settings-view/src/parts/Clear/Clear.ts
@@ -1,10 +1,15 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { Script } from '../InputSource/InputSource.ts'
 
 export const clear = (state: SettingsState): SettingsState => {
+  const { items, tabs } = state
+  const newSearchValue = ''
+  const filteredItems = getFilteredItems(items, tabs, newSearchValue)
   return {
     ...state,
-    searchValue: '',
+    searchValue: newSearchValue,
     inputSource: Script,
+    filteredItems,
   }
 }

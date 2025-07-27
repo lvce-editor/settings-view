af55a6938a3de2480e0656c58e8c1dc1f78b32be
Le Vivilet <levivlet@pm.me>
2025-07-27 10:49:18 +0200

    feature: add createDefaultState

---
+++ packages/settings-view/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -0,0 +1,16 @@
+import type { SettingsState } from '../SettingsState/SettingsState.ts'
+
+export const createDefaultState = (): SettingsState => {
+  return {
+    breakPointsExpanded: false,
+    breakPointsVisible: true,
+    focus: 0,
+    id: 1,
+    uri: 'test://uri',
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 600,
+    tabs: [],
+  }
+}

8b9a701b53fee3574255d92283e594d3f4744782
Le Vivilet <levivlet@pm.me>
2025-07-27 22:47:04 +0200

    feature: restore selected tab id

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -6,16 +6,18 @@ import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
 import { getTabs } from '../GetTabs/GetTabs.ts'
 import { Script } from '../InputSource/InputSource.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
+import { getUpdatedTabs } from '../GetUpdatedTabs/GetUpdatedTabs.ts'
 
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
-  const { searchValue } = restoreState(savedState)
+  const { searchValue, tabId } = restoreState(savedState)
   const tabs = getTabs()
+  const newTabs = getUpdatedTabs(tabs, tabId)
   const items: readonly SettingItem[] = await getSettingItems()
-  const filteredItems = getFilteredItems(items, tabs, searchValue)
+  const filteredItems = getFilteredItems(items, newTabs, searchValue)
   const preferences = await getPreferences()
   return {
     ...state,
-    tabs,
+    tabs: newTabs,
     items,
     filteredItems,
     preferences,

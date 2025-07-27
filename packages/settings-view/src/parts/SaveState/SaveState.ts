91bba3eab48bf48af1071b42b19a0540c06c44cc
Le Vivilet <levivlet@pm.me>
2025-07-27 17:33:13 +0200

    test

--- packages/settings-view/src/parts/SaveState/SaveState.ts
+++ packages/settings-view/src/parts/SaveState/SaveState.ts
@@ -1,12 +1,17 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
-import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { get } from '../SettingsStates/SettingsStates.ts'
 
-export const saveState = (state: SettingsState): SavedState => {
+export const saveState = (uid: number): SavedState => {
+  const { newState } = get(uid)
+  const selectedTab = newState.tabs.find((tab) => tab.selected)?.id || ''
+
   return {
     expandedPaths: [],
     root: '',
     minLineY: 0,
     maxLineY: 0,
     deltaY: 0,
+    searchValue: newState.searchValue,
+    selectedTab,
   }
 }

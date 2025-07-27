f0b1a1b80047e862f3c7d8e775d9c0d35c478845
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 23:22:59 +0200

    feature: add modified property (#170)

--- packages/settings-view/src/parts/SettingItem/SettingItem.ts
+++ packages/settings-view/src/parts/SettingItem/SettingItem.ts
@@ -8,9 +8,9 @@ export interface SettingItem {
   readonly heading: string
   readonly description: string
   readonly type: number
-  readonly value: string // TODO
+  readonly value: string // TODO support different value types
   readonly category: string
   readonly options?: readonly SettingItemOption[]
-
+  readonly modified?: boolean // TODO maybe have two types of settings: one with the modified property and one without
   // TODO add defaultValue property
 }

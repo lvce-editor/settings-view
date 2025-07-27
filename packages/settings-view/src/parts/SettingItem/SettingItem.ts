20a6964e4601551f29a056f4d58d0b46fd45bc29
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-27 15:07:31 +0200

    feature: add setting id property (#92)
    * feature: add setting id property
    
    * test

--- packages/settings-view/src/parts/SettingItem/SettingItem.ts
+++ packages/settings-view/src/parts/SettingItem/SettingItem.ts
@@ -1,9 +1,9 @@
 export interface SettingItem {
+  readonly id: string
   readonly heading: string
   readonly description: string
   readonly type: number
   readonly value: string // TODO
 
   // TODO add defaultValue property
-  // TODO add setting id property
 }

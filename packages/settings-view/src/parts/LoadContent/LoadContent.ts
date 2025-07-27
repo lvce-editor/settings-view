ffcbdf74ee176f16eee7861f0b7d456756231015
Le Vivilet <levivlet@pm.me>
2025-07-27 11:17:04 +0200

    feature: load tabs in loadContent

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -1,7 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { getTabs } from '../GetTabs/GetTabs.ts'
 
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
+  const tabs = getTabs()
   return {
     ...state,
+    tabs,
   }
 }

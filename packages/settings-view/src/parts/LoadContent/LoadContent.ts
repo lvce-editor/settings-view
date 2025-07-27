2434651c6c9a3759081136034fda503b12deeff5
Le Vivilet <levivlet@pm.me>
2025-07-27 22:23:11 +0200

    feature: mark input source as script in loadContent

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -4,6 +4,7 @@ import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { getPreferences } from '../GetPreferences/GetPreferences.ts'
 import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
 import { getTabs } from '../GetTabs/GetTabs.ts'
+import { Script } from '../InputSource/InputSource.ts'
 
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
   const { searchValue } = state
@@ -17,5 +18,6 @@ export const loadContent = async (state: SettingsState, savedState: unknown): Pr
     items,
     filteredItems,
     preferences,
+    inputSource: Script,
   }
 }

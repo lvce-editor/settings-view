0e732f6cdb8142a34f236690fb4e31cdfc313499
Le Vivilet <levivlet@pm.me>
2025-07-28 17:56:21 +0200

    lint

--- packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
@@ -2,8 +2,8 @@ import type { Renderer } from '../Renderer/Renderer.ts'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
-import { renderValue } from '../RenderValue/RenderValue.ts'
 import { renderSettingValues } from '../RenderSettingValues/RenderSettingValues.ts'
+import { renderValue } from '../RenderValue/RenderValue.ts'
 
 export const getRenderer = (diffType: number): Renderer<SettingsState> => {
   switch (diffType) {

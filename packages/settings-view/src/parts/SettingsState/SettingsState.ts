3b263df76d27a35ae70c530f5b15dd2871cd7eaa
Le Vivilet <levivlet@pm.me>
2025-07-28 21:38:51 +0200

    feature; move modifiedSettings into separate file

--- packages/settings-view/src/parts/SettingsState/SettingsState.ts
+++ packages/settings-view/src/parts/SettingsState/SettingsState.ts
@@ -1,9 +1,6 @@
+import type { ModifiedSettings } from '../ModifiedSettings/ModifiedSettings.ts'
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
 import type { Tab } from '../Tab/Tab.ts'
-
-export interface ModifiedSettings {
-  readonly [key: string]: boolean
-}
 
 export interface SettingsState {
   readonly breakPointsExpanded: boolean
@@ -12,17 +9,17 @@ export interface SettingsState {
   readonly filteredItemsCount: number
   readonly focus: number
   readonly height: number
+  readonly history: readonly string[]
   readonly id: number
   readonly inputSource: number
   readonly items: readonly SettingItem[]
+  readonly modifiedSettings: ModifiedSettings
   readonly preferences: any
+  readonly scrollOffset: number
   readonly searchValue: string
   readonly tabs: readonly Tab[]
   readonly uri: string
   readonly width: number
   readonly x: number
   readonly y: number
-  readonly scrollOffset: number
-  readonly history: readonly string[]
-  readonly modifiedSettings: ModifiedSettings
 }

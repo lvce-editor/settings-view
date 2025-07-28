cd04ef5cc806c4c9ec647874828aaca91ff61638
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 17:39:44 +0200

    feature: update input dom to only include input field (#182)

--- packages/settings-view/src/parts/GetSettingsInputDom/GetSettingsInputDom.ts
+++ packages/settings-view/src/parts/GetSettingsInputDom/GetSettingsInputDom.ts
@@ -2,19 +2,12 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getSettingsInputBadgeDom } from '../GetSettingsInputBadgeDom/GetSettingsInputBadgeDom.ts'
-import { getSettingsInputButtonsDom } from '../GetSettingsInputButtonsDom/GetSettingsInputButtonsDom.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as SettingStrings from '../SettingStrings/SettingStrings.ts'
 
-export const getSettingsInputDom = (filteredSettingsCount: number): readonly VirtualDomNode[] => {
+export const getSettingsInputDom = (): readonly VirtualDomNode[] => {
   const placeholder = SettingStrings.searchSettings()
   return [
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.SettingsInputWrapper,
-      childCount: 3,
-    },
     {
       type: VirtualDomElements.Input,
       className: mergeClassNames(ClassNames.InputBox, ClassNames.SettingsSearchInput),
@@ -23,7 +16,5 @@ export const getSettingsInputDom = (filteredSettingsCount: number): readonly Vir
       name: InputName.SettingsSearch,
       onInput: DomEventListenerFunctions.HandleInput,
     },
-    ...getSettingsInputButtonsDom(),
-    ...getSettingsInputBadgeDom(filteredSettingsCount),
   ]
 }

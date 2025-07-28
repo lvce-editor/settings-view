da92b50f30a361ea64832101ee3f1852b8d2ecca
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 21:38:26 +0200

    feature: set modifiedSettings in loadContent (#211)

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -1,5 +1,5 @@
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
-import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import type { ModifiedSettings, SettingsState } from '../SettingsState/SettingsState.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { getPreferences } from '../GetPreferences/GetPreferences.ts'
 import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
@@ -8,20 +8,30 @@ import { getUpdatedTabs } from '../GetUpdatedTabs/GetUpdatedTabs.ts'
 import { Script } from '../InputSource/InputSource.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
 
+const getModifiedSettings = (preferences: any): ModifiedSettings => {
+  const modifiedSettings = Object.create(null)
+  for (const key of Object.keys(preferences)) {
+    modifiedSettings[key] = true
+  }
+  return modifiedSettings
+}
+
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
   const { searchValue, tabId } = restoreState(savedState)
   const tabs = getTabs()
   const newTabs = getUpdatedTabs(tabs, tabId)
   const items: readonly SettingItem[] = await getSettingItems()
   const preferences = await getPreferences()
+  const modifiedSettings: ModifiedSettings = getModifiedSettings(preferences)
   const filteredItems = getFilteredItems(items, newTabs, searchValue, preferences)
   return {
     ...state,
-    tabs: newTabs,
-    items,
     filteredItems,
-    preferences,
     inputSource: Script,
+    items,
+    modifiedSettings,
+    preferences,
     searchValue,
+    tabs: newTabs,
   }
 }

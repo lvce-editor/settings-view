c8f936e644cf5fd81592f81d281b66819062c5a7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 17:12:00 +0200

    feature: use i18n string for matching settings string (#174)

--- packages/settings-view/src/parts/GetSettingsInputBadgeDom/GetSettingsInputBadgeDom.ts
+++ packages/settings-view/src/parts/GetSettingsInputBadgeDom/GetSettingsInputBadgeDom.ts
@@ -1,9 +1,10 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { matchingSettings } from '../SettingStrings/SettingStrings.ts'
 
 export const getSettingsInputBadgeDom = (filteredSettingsCount: number): readonly VirtualDomNode[] => {
-  const badgeText = `${filteredSettingsCount} matching settings`
+  const badgeText = matchingSettings(filteredSettingsCount)
   return [
     {
       type: VirtualDomElements.Div,

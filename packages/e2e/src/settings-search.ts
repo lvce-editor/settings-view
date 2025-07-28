2d328223ec270bf2504d2e849973cca505decc95
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 21:24:28 +0200

    feature: add search e2e test (#204)

---
+++ packages/e2e/src/settings-search.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.search'
+
+export const test: Test = async ({ Main, Locator, expect, Command }) => {
+  // arrange
+  await Main.openUri('settings://') // TODO add page object
+
+  // act
+  await Command.execute('Settings.handleInput', 'font family', 2) // TODO page object
+
+  // assert
+  const settingsItems = Locator('.SettingsItem')
+  await expect(settingsItems).toHaveCount(1)
+  const heading = settingsItems.locator('h3')
+  await expect(heading).toHaveText('Font Family')
+  const description = settingsItems.locator('p')
+  await expect(description).toHaveText('The font family of the editor')
+  const input = settingsItems.locator('.InputBox')
+  await expect(input).toHaveValue('Fira Code')
+}

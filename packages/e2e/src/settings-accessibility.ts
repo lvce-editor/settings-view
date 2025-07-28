d0bf6008a76e7dac899b76865055895ffcc9d2b2
Le Vivilet <levivlet@pm.me>
2025-07-28 23:46:10 +0200

    feature: add accessibility e2e test

---
+++ packages/e2e/src/settings-accessibility.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.accessibility'
+
+export const test: Test = async ({ Main, Locator, expect }) => {
+  // arrange
+
+  // act
+  await Main.openUri('settings://') // TODO add page object
+
+  // assert
+  const settingsInput = Locator('.SetingsSearchInput')
+  await expect(settingsInput).toHaveAttribute('placeholder', 'Search Settings')
+  // TODO should input it have type search? probably
+  const settingsTabs = Locator('.SettingsTabs')
+  await expect(settingsTabs).toHaveAttribute('role', 'tablist')
+  const tabTextEditor = Locator('.SettingsTab[name="text-editor"]')
+  await expect(tabTextEditor).toHaveAttribute('role', 'tab')
+
+  // TODO test clear button accessibility
+  // TODO test badge accesibility, it should have aria-live polite
+}

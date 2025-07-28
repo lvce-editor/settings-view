9aba064d1f2cd1bb664763059a155b10fd4e06dc
Le Vivilet <levivlet@pm.me>
2025-07-28 17:17:58 +0200

    lint

--- packages/settings-view/test/GetSettingsNoResultsDom.test.ts
+++ packages/settings-view/test/GetSettingsNoResultsDom.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import * as GetSettingsNoResultsDom from '../src/parts/GetSettingsNoResultsDom/GetSettingsNoResultsDom.ts'
 import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetSettingsNoResultsDom from '../src/parts/GetSettingsNoResultsDom/GetSettingsNoResultsDom.ts'
 import * as SettingStrings from '../src/parts/SettingStrings/SettingStrings.ts'
 
 test('getSettingsNoResultsDom returns correct virtual DOM structure', () => {

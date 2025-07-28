891ce93e5ffc4bf60f42f557de17d258e815f9ae
Le Vivilet <levivlet@pm.me>
2025-07-28 21:30:23 +0200

    feature: add clear e2e test

---
+++ packages/e2e/src/settings-search-clear.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.search-clear'
+
+export const test: Test = async ({ Main, Locator, expect, Command }) => {
+  // arrange
+  await Main.openUri('settings://') // TODO add page object
+  await Command.execute('Settings.handleInput', 'font family', 2) // TODO page object
+
+  // act
+  await Command.execute('Settings.clear', '')
+
+  // assert
+  const input = Locator('.SettingsSearchInput')
+  await expect(input).toHaveValue('')
+  const wordWrapSetting = Locator('[name="Editor.wordWrap"]')
+  await expect(wordWrapSetting).toBeVisible()
+}

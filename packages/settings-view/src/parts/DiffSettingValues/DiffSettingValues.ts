8d27c4a5117e16f44d6aeca31a4fe87f66788e2d
Le Vivilet <levivlet@pm.me>
2025-07-28 18:08:24 +0200

    feature: simplify settings diff

--- packages/settings-view/src/parts/DiffSettingValues/DiffSettingValues.ts
+++ packages/settings-view/src/parts/DiffSettingValues/DiffSettingValues.ts
@@ -1,19 +1,5 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const isEqual = (oldState: SettingsState, newState: SettingsState): boolean => {
-  if (oldState.filteredItems === newState.filteredItems) {
-    return true
-  }
-
-  if (oldState.filteredItems.length !== newState.filteredItems.length) {
-    return false
-  }
-
-  for (let i = 0; i < oldState.filteredItems.length; i++) {
-    if (oldState.filteredItems[i] !== newState.filteredItems[i]) {
-      return false
-    }
-  }
-
-  return true
+  return oldState.filteredItems === newState.filteredItems
 }

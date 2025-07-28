acc36931079bca6282979e58647d26534cd7548b
Le Vivilet <levivlet@pm.me>
2025-07-28 17:19:50 +0200

    feature: add modified indicator dom

---
+++ packages/settings-view/src/parts/GetSettingsModifiedIndicatorDom/GetSettingsModifiedIndicatorDom.ts
@@ -0,0 +1,13 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+
+export const getSettingsModifiedIndicatorDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ModifiedIndicator,
+      childCount: 1,
+    },
+  ]
+}

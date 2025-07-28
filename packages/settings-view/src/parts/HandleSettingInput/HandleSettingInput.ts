f055de68adcea4c7d0e9163c7297cf34da548d5e
Le Vivilet <levivlet@pm.me>
2025-07-28 21:39:12 +0200

    feature: add input source for setting input

--- packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
+++ packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
@@ -1,8 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { User } from '../InputSource/InputSource.ts'
 
-export const handleSettingInput = (state: SettingsState, name: string, value: string): SettingsState => {
+export const handleSettingInput = (state: SettingsState, name: string, value: string, inputSource = User): SettingsState => {
   // TODO update value
   return {
     ...state,
+    inputSource,
   }
 }

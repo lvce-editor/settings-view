80235dfb1bfca7fd2dccfb167008b0ec7b49e57d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 18:34:18 +0200

    feature: refactor item dom (#193)

--- packages/settings-view/src/parts/GetItemVirtualDom/GetItemVirtualDom.ts
+++ packages/settings-view/src/parts/GetItemVirtualDom/GetItemVirtualDom.ts
@@ -1,9 +1,10 @@
-import { type VirtualDomNode, text } from '@lvce-editor/virtual-dom-worker'
+import { type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
 import { getItemCheckBoxVirtualDom } from '../GetItemCheckBoxVirtualDom/GetItemCheckBoxVirtualDom.ts'
 import { getItemNumberVirtualDom } from '../GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts'
 import { getItemSelectVirtualDom } from '../GetItemSelectVirtualDom/GetItemSelectVirtualDom.ts'
 import { getItemStringVirtualDom } from '../GetItemStringVirtualDom/GetItemStringVirtualDom.ts'
+import { getItemUnknownVirtualDom } from '../GetItemUnknownVirtualDom/GetItemUnknownVirtualDom.ts'
 import * as SettingItemType from '../SettingItemType/SettingItemType.ts'
 
 export const getItemVirtualDom = (item: SettingItem): readonly VirtualDomNode[] => {
@@ -19,6 +20,5 @@ export const getItemVirtualDom = (item: SettingItem): readonly VirtualDomNode[] 
   if (item.type === SettingItemType.Enum) {
     return getItemSelectVirtualDom(item)
   }
-  // TODO
-  return [text('unknown setting type')]
+  return getItemUnknownVirtualDom()
 }

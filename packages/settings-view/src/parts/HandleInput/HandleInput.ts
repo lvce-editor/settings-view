2b5cc7a40996d849c759da7fd022f47faa562de5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-28 21:15:48 +0200

    feature: make input source configurable (#201)

--- packages/settings-view/src/parts/HandleInput/HandleInput.ts
+++ packages/settings-view/src/parts/HandleInput/HandleInput.ts
@@ -2,13 +2,13 @@ import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { User } from '../InputSource/InputSource.ts'
 
-export const handleInput = (state: SettingsState, value: string): SettingsState => {
+export const handleInput = (state: SettingsState, value: string, inputSource = User): SettingsState => {
   const { items, tabs, preferences } = state
   const filteredItems = getFilteredItems(items, tabs, value, preferences)
   return {
     ...state,
     searchValue: value,
     filteredItems,
-    inputSource: User,
+    inputSource,
   }
 }

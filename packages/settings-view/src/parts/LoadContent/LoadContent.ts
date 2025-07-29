51658fc2e40ad478ae23b90ce8bdb38604327eed
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 11:48:39 +0200

    feature: restore scrolltop in loadContent (#255)

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -18,7 +18,7 @@ const getModifiedSettings = (preferences: any): ModifiedSettings => {
 }
 
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
-  const { searchValue, tabId } = restoreState(savedState)
+  const { searchValue, tabId, scrollOffset } = restoreState(savedState)
   const tabs = getTabs()
   const newTabs = getUpdatedTabs(tabs, tabId)
   const items: readonly SettingItem[] = await getSettingItems()
@@ -32,6 +32,7 @@ export const loadContent = async (state: SettingsState, savedState: unknown): Pr
     items,
     modifiedSettings,
     preferences,
+    scrollOffset,
     searchValue,
     tabs: newTabs,
   }

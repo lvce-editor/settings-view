0d3075c2d07745c654bfe983656dcbc2836841eb
Le Vivilet <levivlet@pm.me>
2025-07-29 23:09:55 +0200

    feature: add extensions tab e2e test

---
+++ packages/e2e/src/settings-select-extensions.ts
@@ -0,0 +1,15 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.select-extensions'
+
+export const test: Test = async ({ Main, Locator, expect, Command }) => {
+  // arrange
+  await Main.openUri('settings://') // TODO add page object
+
+  // act
+  await Command.execute('Settings.handleClickTab', 'extensions')
+
+  // assert
+  const heading = Locator('.SettingsContentHeading')
+  await expect(heading).toHaveText('extensions')
+}

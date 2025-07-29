ffb645a2d714dbc3f16ff2ac6bcd8937ce0b92a1
Le Vivilet <levivlet@pm.me>
2025-07-29 11:25:16 +0200

    feature: add handleSettingUpdate helper function

--- packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
+++ packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
@@ -1,20 +1,7 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
-import { getNewFilteredItems } from '../GetNewFilteredItems/GetNewFilteredItems.ts'
-import { getNewModifiedSettings } from '../GetNewModifiedSettings/GetNewModifiedSettings.ts'
+import { handleSettingUpdate } from '../HandleSettingUpdate/HandleSettingUpdate.ts'
 import { User } from '../InputSource/InputSource.ts'
 
 export const handleSettingInput = (state: SettingsState, name: string, value: string, inputSource = User): SettingsState => {
-  const { modifiedSettings, items, tabs, searchValue, preferences, filteredItems } = state
-  const newModifiedSettings = getNewModifiedSettings(modifiedSettings, name)
-  const newPreferences = {
-    ...preferences,
-    [name]: value,
-  }
-  const newFilteredItems = getNewFilteredItems(modifiedSettings, newModifiedSettings, items, tabs, searchValue, filteredItems)
-  return {
-    ...state,
-    inputSource,
-    filteredItems: newFilteredItems,
-    preferences: newPreferences,
-  }
+  return handleSettingUpdate(state, name, value, inputSource)
 }

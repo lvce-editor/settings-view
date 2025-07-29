f4475a730efcb722432df886409475ae003db297
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 19:06:47 +0200

    feature: render focus context (#302)
    * feature: render focus context
    
    * skip

--- packages/settings-view/src/parts/DiffModules/DiffModules.ts
+++ packages/settings-view/src/parts/DiffModules/DiffModules.ts
@@ -5,6 +5,20 @@ import * as DiffSettingValues from '../DiffSettingValues/DiffSettingValues.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffFocus.isEqual, DiffItems.isEqual, DiffValue.isEqual, DiffSettingValues.isEqual, DiffScrollOffset.isEqual]
+export const modules = [
+  DiffItems.isEqual,
+  DiffValue.isEqual,
+  DiffSettingValues.isEqual,
+  DiffScrollOffset.isEqual,
+  DiffFocus.isEqual,
+  DiffFocus.isEqual,
+]
 
-export const numbers = [DiffType.RenderFocus, DiffType.RenderItems, DiffType.RenderValue, DiffType.RenderSettingValues, DiffType.RenderScrollOffset]
+export const numbers = [
+  DiffType.RenderItems,
+  DiffType.RenderValue,
+  DiffType.RenderSettingValues,
+  DiffType.RenderScrollOffset,
+  DiffType.RenderFocus,
+  DiffType.RenderFocusContext,
+]

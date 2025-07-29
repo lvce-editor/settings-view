46b139ab6636cbe58ead3ff4d461c3b9d721e5df
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 22:58:56 +0200

    feature: hoist parent node (#313)

--- packages/settings-view/src/parts/GetItemHeadingDom/GetItemHeadingDom.ts
+++ packages/settings-view/src/parts/GetItemHeadingDom/GetItemHeadingDom.ts
@@ -1,12 +1,11 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
+const parent: VirtualDomNode = {
+  type: VirtualDomElements.H3,
+  childCount: 1,
+}
+
 export const getItemHeadingDom = (heading: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.H3,
-      childCount: 1,
-    },
-    text(heading),
-  ]
+  return [parent, text(heading)]
 }

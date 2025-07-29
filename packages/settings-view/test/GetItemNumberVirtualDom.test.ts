28d06090a814f82ea9f480cf900a24ed8ffff7ec
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 18:52:21 +0200

    feature: render modified indicator (#296)
    * feature: render modified indicator
    
    * memory

--- packages/settings-view/test/GetItemNumberVirtualDom.test.ts
+++ packages/settings-view/test/GetItemNumberVirtualDom.test.ts
@@ -49,3 +49,56 @@ test('getItemNumberVirtualDom returns expected DOM structure for normal item', (
 
   expect(virtualDom).toEqual(expectedDom)
 })
+
+test('getItemNumberVirtualDom returns expected DOM structure for modified item', () => {
+  const item = {
+    id: 'testItem',
+    heading: 'Test Heading',
+    description: 'Test Description',
+    type: 1,
+    value: '42',
+    category: 'test',
+    modified: true,
+  }
+
+  const virtualDom = getItemNumberVirtualDom(item)
+
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: 'SettingsItem',
+      childCount: 4,
+      role: 'group',
+      'data-modified': true,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'ModifiedIndicator',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.H3,
+      childCount: 1,
+    },
+    text('Test Heading'),
+    {
+      type: VirtualDomElements.Label,
+      htmlFor: 'testItem',
+      childCount: 1,
+      className: 'Label',
+    },
+    text('Test Description'),
+    {
+      type: VirtualDomElements.Input,
+      className: 'InputBox',
+      inputType: 'number',
+      placeholder: SettingStrings.numberValue(),
+      childCount: 0,
+      id: 'testItem',
+      name: 'testItem',
+      onInput: 'handleSettingInput',
+    },
+  ]
+
+  expect(virtualDom).toEqual(expectedDom)
+})

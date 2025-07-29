567b5a23e93ba99232427de1ad04ada332356be6
Le Vivilet <levivlet@pm.me>
2025-07-29 19:25:58 +0200

    feature: move function into separate file

--- packages/settings-view/src/parts/LoadContent/LoadContent.ts
+++ packages/settings-view/src/parts/LoadContent/LoadContent.ts
@@ -2,20 +2,13 @@ import type { ModifiedSettings } from '../ModifiedSettings/ModifiedSettings.ts'
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
+import { getModifiedSettings } from '../GetModifiedSettings/GetModifiedSettings.ts'
 import { getPreferences } from '../GetPreferences/GetPreferences.ts'
 import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
 import { getTabs } from '../GetTabs/GetTabs.ts'
 import { getUpdatedTabs } from '../GetUpdatedTabs/GetUpdatedTabs.ts'
 import { Script } from '../InputSource/InputSource.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
-
-const getModifiedSettings = (preferences: any): ModifiedSettings => {
-  const modifiedSettings = Object.create(null)
-  for (const key of Object.keys(preferences)) {
-    modifiedSettings[key] = true
-  }
-  return modifiedSettings
-}
 
 export const loadContent = async (state: SettingsState, savedState: unknown): Promise<SettingsState> => {
   const { searchValue, tabId, scrollOffset } = restoreState(savedState)

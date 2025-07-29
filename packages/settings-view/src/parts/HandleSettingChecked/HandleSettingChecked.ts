66204ca8d6dd697935fbcddba4956d3cfb8d8ba4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 09:52:48 +0200

    feature: update items modified state when checking checkbox (#232)

--- packages/settings-view/src/parts/HandleSettingChecked/HandleSettingChecked.ts
+++ packages/settings-view/src/parts/HandleSettingChecked/HandleSettingChecked.ts
@@ -1,8 +1,18 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 
 export const handleSettingChecked = (state: SettingsState, name: string, value: string): SettingsState => {
+  const { modifiedSettings, items, tabs, searchValue } = state
+  const newModifiedSettings = {
+    ...modifiedSettings,
+    [name]: true,
+  }
+  const filteredItems = getFilteredItems(items, tabs, searchValue, newModifiedSettings)
+
   // TODO update value
   return {
     ...state,
+    modifiedSettings: newModifiedSettings,
+    filteredItems,
   }
 }

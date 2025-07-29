ee43219ad0d64785a9726c3a4d1ec109995e68b5
Le Vivilet <levivlet@pm.me>
2025-07-29 11:33:20 +0200

    feature: add unknown dom test

---
+++ packages/settings-view/test/GetItemUnknownVirtualDom.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@jest/globals'
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import { getItemUnknownVirtualDom } from '../src/parts/GetItemUnknownVirtualDom/GetItemUnknownVirtualDom.ts'
+import * as SettingStrings from '../src/parts/SettingStrings/SettingStrings.ts'
+
+test('getItemUnknownVirtualDom returns correct virtual DOM structure', () => {
+  const result = getItemUnknownVirtualDom()
+
+  expect(result).toHaveLength(2)
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.SettingsItem,
+    childCount: 1,
+    role: 'group',
+  })
+  expect(result[1]).toEqual(text(SettingStrings.unknownSettingType()))
+})

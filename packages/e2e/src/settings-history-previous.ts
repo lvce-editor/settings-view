979d22ce23ecae00c65701038e6c4f0d9c0bf733
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 19:24:31 +0200

    feature: add previous history test (#307)

---
+++ packages/e2e/src/settings-history-previous.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.history-previous'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, Command }) => {
+  // arrange
+  await Main.openUri('settings://') // TODO add page object
+  await Command.execute('Settings.clearHistory')
+  await Command.execute('Settings.handleInput', 'font family', 2) // TODO page object
+  await Command.execute('Settings.handleInput', 'font size', 2) // TODO page object
+
+  // act
+  await Command.execute('Settings.usePreviousSearchValue') // TODO page object
+
+  // assert
+  const input = Locator('.SettingsSearchInput')
+  await expect(input).toHaveValue('font family')
+  await expect(input).toBeFocused()
+  const wordWrapSetting = Locator('[name="Editor.wordWrap"]')
+  await expect(wordWrapSetting).toBeVisible()
+}

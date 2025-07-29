71d8e765903e865ec77f49c9a149c828f97e0ebb
Le Vivilet <levivlet@pm.me>
2025-07-29 23:39:45 +0200

    feature: add displayitem type

---
+++ packages/settings-view/src/parts/DisplaySettingItem/DisplaySettingItem.ts
@@ -0,0 +1,17 @@
+export interface DisplaySettingItemOption {
+  readonly id: string
+  readonly label: string
+}
+
+export interface DisplaySettingItem {
+  readonly id: string
+  readonly heading: string
+  readonly description: string
+  readonly type: number
+  readonly value: any
+  readonly category: string
+  readonly options?: readonly DisplaySettingItemOption[]
+  readonly modified: boolean
+  readonly errorMessage: string
+  readonly hasError: boolean
+}

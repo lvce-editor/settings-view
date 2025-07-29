008185805a95a690f054983f4fcea0600845dd5d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 11:53:56 +0200

    feature: update diffmodules (#258)

--- packages/settings-view/src/parts/DiffModules/DiffModules.ts
+++ packages/settings-view/src/parts/DiffModules/DiffModules.ts
@@ -1,9 +1,10 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffScrollOffset from '../DiffScrollOffset/DiffScrollOffset.ts'
 import * as DiffSettingValues from '../DiffSettingValues/DiffSettingValues.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
-export const modules = [DiffFocus.isEqual, DiffItems.isEqual, DiffValue.isEqual, DiffSettingValues.isEqual]
+export const modules = [DiffFocus.isEqual, DiffItems.isEqual, DiffValue.isEqual, DiffSettingValues.isEqual, DiffScrollOffset.isEqual]
 
-export const numbers = [DiffType.RenderFocus, DiffType.RenderItems, DiffType.RenderValue, DiffType.RenderSettingValues]
+export const numbers = [DiffType.RenderFocus, DiffType.RenderItems, DiffType.RenderValue, DiffType.RenderSettingValues, DiffType.RenderScrollOffset]

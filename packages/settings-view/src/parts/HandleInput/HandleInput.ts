ebc756690e0ea044df7103f916246ef25a52d88c
Le Vivilet <levivlet@pm.me>
2025-07-29 12:11:17 +0200

    lint

--- packages/settings-view/src/parts/HandleInput/HandleInput.ts
+++ packages/settings-view/src/parts/HandleInput/HandleInput.ts
@@ -1,7 +1,7 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { addToHistory } from '../AddToHistory/AddToHistory.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { User } from '../InputSource/InputSource.ts'
-import { addToHistory } from '../AddToHistory/AddToHistory.ts'
 
 export const handleInput = (state: SettingsState, value: string, inputSource = User): SettingsState => {
   const { items, tabs, preferences, history } = state

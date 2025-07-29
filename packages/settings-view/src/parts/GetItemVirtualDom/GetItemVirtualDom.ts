0f1b6c43109b19162a2db80e8b590dbe5fc62b33
Le Vivilet <levivlet@pm.me>
2025-07-29 16:25:19 +0200

    register

--- packages/settings-view/src/parts/GetItemVirtualDom/GetItemVirtualDom.ts
+++ packages/settings-view/src/parts/GetItemVirtualDom/GetItemVirtualDom.ts
@@ -1,12 +1,12 @@
 import { type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { SettingItem } from '../SettingItem/SettingItem.ts'
 import { getItemCheckBoxVirtualDom } from '../GetItemCheckBoxVirtualDom/GetItemCheckBoxVirtualDom.ts'
+import { getItemColorVirtualDom } from '../GetItemColorVirtualDom/GetItemColorVirtualDom.ts'
 import { getItemNumberVirtualDom } from '../GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts'
 import { getItemSelectVirtualDom } from '../GetItemSelectVirtualDom/GetItemSelectVirtualDom.ts'
 import { getItemStringVirtualDom } from '../GetItemStringVirtualDom/GetItemStringVirtualDom.ts'
 import { getItemUnknownVirtualDom } from '../GetItemUnknownVirtualDom/GetItemUnknownVirtualDom.ts'
 import * as SettingItemType from '../SettingItemType/SettingItemType.ts'
-import { getItemColorVirtualDom } from '../GetItemColorVirtualDom/GetItemColorVirtualDom.ts'
 
 export const getItemVirtualDom = (item: SettingItem): readonly VirtualDomNode[] => {
   if (item.type === SettingItemType.Number) {

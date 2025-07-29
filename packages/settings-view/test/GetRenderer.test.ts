017b13c27f634edc1fe743e8ccd3b176fed56f0a
Le Vivilet <levivlet@pm.me>
2025-07-29 11:33:35 +0200

    feature: add getRenderer test

--- packages/settings-view/test/GetRenderer.test.ts
+++ packages/settings-view/test/GetRenderer.test.ts
@@ -2,64 +2,24 @@ import { test, expect } from '@jest/globals'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
 import { getRenderer } from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
+import { renderSettingValues } from '../src/parts/RenderSettingValues/RenderSettingValues.ts'
+import { renderValue } from '../src/parts/RenderValue/RenderValue.ts'
 
-test('getRenderer returns renderItems for RenderItems diff type', () => {
+test('getRenderer returns renderItems for DiffType.RenderItems', () => {
   const renderer = getRenderer(DiffType.RenderItems)
-
-  expect(typeof renderer).toBe('function')
   expect(renderer).toBe(RenderItems.renderItems)
 })
 
+test('getRenderer returns renderValue for DiffType.RenderValue', () => {
+  const renderer = getRenderer(DiffType.RenderValue)
+  expect(renderer).toBe(renderValue)
+})
+
+test('getRenderer returns renderSettingValues for DiffType.RenderSettingValues', () => {
+  const renderer = getRenderer(DiffType.RenderSettingValues)
+  expect(renderer).toBe(renderSettingValues)
+})
+
 test('getRenderer throws error for unknown diff type', () => {
-  expect(() => {
-    getRenderer(999)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderFocus diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderFocus)
-  }).toThrow('unknown renderer')
-})
-
-test.skip('getRenderer throws error for RenderValue diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderValue)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderReplaceValue diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderReplaceValue)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderIncludeValue diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderIncludeValue)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderExcludeValue diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderExcludeValue)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderFocusContext diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderFocusContext)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderSelection diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderSelection)
-  }).toThrow('unknown renderer')
-})
-
-test('getRenderer throws error for RenderPauseOnExceptions diff type', () => {
-  expect(() => {
-    getRenderer(DiffType.RenderPauseOnExceptions)
-  }).toThrow('unknown renderer')
+  expect(() => getRenderer(999)).toThrow('unknown renderer')
 })

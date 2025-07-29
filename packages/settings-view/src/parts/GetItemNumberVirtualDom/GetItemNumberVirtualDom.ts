79313111cf2753dd5650a30b822c72bfc28c0b49
Le Vivilet <levivlet@pm.me>
2025-07-29 18:48:19 +0200

    feature: render modified indicator

--- packages/settings-view/src/parts/GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts
+++ packages/settings-view/src/parts/GetItemNumberVirtualDom/GetItemNumberVirtualDom.ts
@@ -6,20 +6,23 @@ import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEven
 import { getInputId } from '../GetInputId/GetInputId.ts'
 import { getItemHeadingDom } from '../GetItemHeadingDom/GetItemHeadingDom.ts'
 import { getItemLabelDom } from '../GetItemLabelDom/GetItemLabelDom.ts'
+import { getSettingsModifiedIndicatorDom } from '../GetSettingsModifiedIndicatorDom/GetSettingsModifiedIndicatorDom.ts'
 import * as SettingStrings from '../SettingStrings/SettingStrings.ts'
 
 export const getItemNumberVirtualDom = (item: SettingItem): readonly VirtualDomNode[] => {
   const { heading, description, id, modified } = item
   const domId = getInputId(id)
   const isModified = modified || false
+  const modifiedChildCount = isModified ? 1 : 0
   return [
     {
       type: VirtualDomElements.Div,
       className: ClassNames.SettingsItem,
-      childCount: 3,
+      childCount: 3 + modifiedChildCount,
       role: 'group',
       'data-modified': isModified,
     },
+    ...getSettingsModifiedIndicatorDom(isModified),
     ...getItemHeadingDom(heading),
     ...getItemLabelDom(domId, description),
     {

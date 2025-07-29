3e27f0188816a044004d8426d39efff99c310237
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 09:55:06 +0200

    feature: update modified indicator on input (#233)

--- packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
+++ packages/settings-view/src/parts/HandleSettingInput/HandleSettingInput.ts
@@ -1,10 +1,21 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
+import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { User } from '../InputSource/InputSource.ts'
 
 export const handleSettingInput = (state: SettingsState, name: string, value: string, inputSource = User): SettingsState => {
   // TODO update value
+  const { modifiedSettings, items, tabs, searchValue } = state
+  const newModifiedSettings = {
+    ...modifiedSettings,
+    [name]: true,
+  }
+
+  // TODO don't need to update items if it was already modified
+  const filteredItems = getFilteredItems(items, tabs, searchValue, newModifiedSettings)
+
   return {
     ...state,
     inputSource,
+    filteredItems,
   }
 }

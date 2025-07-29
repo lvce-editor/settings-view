59c7ac9d9a97472d635579029904170d59a21e1c
Le Vivilet <levivlet@pm.me>
2025-07-29 16:27:42 +0200

    feature: render color initial values

--- packages/settings-view/src/parts/RenderSettingValues/RenderSettingValues.ts
+++ packages/settings-view/src/parts/RenderSettingValues/RenderSettingValues.ts
@@ -2,9 +2,10 @@ import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
 import * as SettingItemType from '../SettingItemType/SettingItemType.ts'
 
+const enabledTypes: readonly number[] = [SettingItemType.Number, SettingItemType.String, SettingItemType.Color]
+
 export const renderSettingValues = (oldState: SettingsState, newState: SettingsState): ViewletCommand => {
   const { filteredItems, id } = newState
-  const enabledTypes = [SettingItemType.Number, SettingItemType.String]
   const numericSettings = filteredItems.filter((item) => enabledTypes.includes(item.type))
   const inputValues = numericSettings.map((item) => ({
     name: item.id,

7562267071d65e83cdcb8b8f55ecd506dc1f9038
Le Vivilet <levivlet@pm.me>
2025-07-29 18:57:05 +0200

    feature: add keybindings

--- packages/settings-view/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/settings-view/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,3 +1,18 @@
-export const getKeyBindings = (): readonly any[] => {
-  return []
+import { KeyCode } from '@lvce-editor/virtual-dom-worker'
+import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const getKeyBindings = (): readonly KeyBinding[] => {
+  return [
+    {
+      key: KeyCode.UpArrow,
+      command: 'Settings.usePreviousSearchValue',
+      when: WhenExpression.FocusSettingsInput,
+    },
+    {
+      key: KeyCode.DownArrow,
+      command: 'Settings.useNextSearchValue',
+      when: WhenExpression.FocusSettingsInput,
+    },
+  ]
 }

1ccc938e38f665d7da3db29151e6e97384c3a0eb
Le Vivilet <levivlet@pm.me>
2025-07-29 11:47:28 +0200

    feature: register scrolltop render

--- packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/settings-view/src/parts/GetRenderer/GetRenderer.ts
@@ -2,6 +2,7 @@ import type { Renderer } from '../Renderer/Renderer.ts'
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
+import { renderScrollOffset } from '../RenderScrollOffset/RenderScrollOffset.ts'
 import { renderSettingValues } from '../RenderSettingValues/RenderSettingValues.ts'
 import { renderValue } from '../RenderValue/RenderValue.ts'
 
@@ -13,6 +14,8 @@ export const getRenderer = (diffType: number): Renderer<SettingsState> => {
       return renderValue
     case DiffType.RenderSettingValues:
       return renderSettingValues
+    case DiffType.RenderScrollOffset:
+      return renderScrollOffset
     default:
       throw new Error('unknown renderer')
   }

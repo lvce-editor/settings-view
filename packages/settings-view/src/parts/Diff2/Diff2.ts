93be76f30b24130a48f4f540818c2106dae38a26
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-29 18:28:43 +0200

    feature: disable flaky scroll restoration for now (#293)

--- packages/settings-view/src/parts/Diff2/Diff2.ts
+++ packages/settings-view/src/parts/Diff2/Diff2.ts
@@ -3,7 +3,6 @@ import * as SettingsStates from '../SettingsStates/SettingsStates.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
   const { oldState, newState } = SettingsStates.get(uid)
-
   const diffResult = Diff.diff(oldState, newState)
   return diffResult
 }

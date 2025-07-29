bd5f5501a1ef89fd855a9174546f35712f8a1709
Le Vivilet <levivlet@pm.me>
2025-07-29 18:27:37 +0200

    feature: disable flaky scroll restoration for now

--- packages/settings-view/src/parts/Diff/Diff.ts
+++ packages/settings-view/src/parts/Diff/Diff.ts
@@ -1,10 +1,16 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import * as DiffModules from '../DiffModules/DiffModules.ts'
+import { RenderScrollOffset } from '../DiffType/DiffType.ts'
 
 export const diff = (oldState: SettingsState, newState: SettingsState): readonly number[] => {
   const diffResult: number[] = []
   for (let i = 0; i < DiffModules.modules.length; i++) {
     const fn = DiffModules.modules[i]
+
+    // TODO enable this
+    if (DiffModules.numbers[i] === RenderScrollOffset) {
+      continue
+    }
     if (!fn(oldState, newState)) {
       diffResult.push(DiffModules.numbers[i])
     }

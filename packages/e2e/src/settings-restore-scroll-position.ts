3863472b1693a18302c3822ddff003b428ee6085
Le Vivilet <levivlet@pm.me>
2025-07-29 13:36:54 +0200

    feature: add scroll restore e2e test

---
+++ packages/e2e/src/settings-restore-scroll-position.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'settings.restore-scroll-position'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, Locator, expect, Command }) => {
+  // arrange
+  await Main.openUri('settings://') // TODO add page object
+  await Command.execute('Settings.handleScroll', 20, 2)
+  await Main.closeAllEditors()
+
+  // act
+  await Main.openUri('settings://') // TODO add page object
+
+  // assert
+  const settingsContent = Locator('.SettingsContent')
+  await expect(settingsContent).toHaveJSProperty('scrollTop', 20)
+}

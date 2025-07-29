417de8e2c578ca094ac80b57b4c6b784f6f3dcf0
Le Vivilet <levivlet@pm.me>
2025-07-29 16:14:52 +0200

    skip

--- packages/e2e/src/settings-search-clear.ts
+++ packages/e2e/src/settings-search-clear.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'settings.search-clear'
 
+export const skip = 1
+
 export const test: Test = async ({ Main, Locator, expect, Command }) => {
   // arrange
   await Main.openUri('settings://') // TODO add page object

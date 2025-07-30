bc57ca59f7f131a98b4e575b7a2478c5252666fe
Le Vivilet <levivlet@pm.me>
2025-07-30 08:47:47 +0200

    tsc

--- packages/settings-view/src/parts/GetSettingsMainDom/GetSettingsMainDom.ts
+++ packages/settings-view/src/parts/GetSettingsMainDom/GetSettingsMainDom.ts
@@ -1,12 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { SettingItem } from '../SettingItem/SettingItem.ts'
-import type { Tab } from '../Tab/Tab.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { DisplaySettingItem } from '../DisplaySettingItem/DisplaySettingItem.ts'
 import { getSettingsContentDom } from '../GetSettingsContentDom/GetSettingsContentDom.ts'
 import { getSettingsSideBarDom } from '../GetSettingsSideBarDom/GetSettingsSideBarDom.ts'
+import type { Tab } from '../Tab/Tab.ts'
 
-export const getSettingsMainDom = (tabs: readonly Tab[], items: readonly SettingItem[], searchValue: string): readonly VirtualDomNode[] => {
+export const getSettingsMainDom = (tabs: readonly Tab[], items: readonly DisplaySettingItem[], searchValue: string): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,

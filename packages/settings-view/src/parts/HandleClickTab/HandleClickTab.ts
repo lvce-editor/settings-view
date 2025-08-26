5cfff1f246601b4590adc4a4803767e44539cc99
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 16:38:11 +0200

    Discard changes to packages/settings-view/src/parts/HandleClickTab/HandleClickTab.ts

--- packages/settings-view/src/parts/HandleClickTab/HandleClickTab.ts
+++ packages/settings-view/src/parts/HandleClickTab/HandleClickTab.ts
@@ -1,5 +1,4 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
-import { computeVisibleItems } from '../ComputeVisibleItems/ComputeVisibleItems.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { getUpdatedTabs } from '../GetUpdatedTabs/GetUpdatedTabs.ts'
 
@@ -8,17 +7,13 @@ export const handleClickTab = (state: SettingsState, name: string): SettingsStat
     return state
   }
 
-  const { items, tabs, searchValue, modifiedSettings, preferences, height, itemHeight, scrollOffset } = state
+  const { items, tabs, searchValue, modifiedSettings, preferences } = state
   const updatedTabs = getUpdatedTabs(tabs, name)
   const filteredItems = getFilteredItems(items, updatedTabs, searchValue, modifiedSettings, preferences)
-  const { visibleItems, minLineY, maxLineY } = computeVisibleItems(filteredItems, height, scrollOffset, itemHeight)
 
   return {
     ...state,
     tabs: updatedTabs,
     filteredItems,
-    visibleItems,
-    minLineY,
-    maxLineY,
   }
 }

359698e2e5afee3d49f0e319056e3adf04dd325c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 23:54:07 +0200

    Discard changes to packages/settings-view/src/parts/RenderItems/RenderItems.ts

--- packages/settings-view/src/parts/RenderItems/RenderItems.ts
+++ packages/settings-view/src/parts/RenderItems/RenderItems.ts
@@ -4,6 +4,5 @@ import { getSettingsDom } from '../GetSettingsDom/GetSettingsDom.ts'
 
 export const renderItems = (oldState: SettingsState, newState: SettingsState): ViewletCommand => {
   const dom = getSettingsDom(newState)
-  console.log({ dom })
   return ['Viewlet.setDom2', newState.id, dom]
 }

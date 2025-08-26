2ad66a04cbf5f26d602ac640ae3d2df6acb812a6
Le Vivilet <levivlet@pm.me>
2025-08-27 00:03:52 +0200

    feature: update scrollbar on input

--- packages/settings-view/src/parts/HandleInput/HandleInput.ts
+++ packages/settings-view/src/parts/HandleInput/HandleInput.ts
@@ -1,11 +1,16 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 import { addToHistory } from '../AddToHistory/AddToHistory.ts'
+import { computeScrollBar } from '../ComputeScrollBar/ComputeScrollBar.ts'
+import { computeVisibleItems } from '../ComputeVisibleItems/ComputeVisibleItems.ts'
 import { getFilteredItems } from '../GetFilteredItems/GetFilteredItems.ts'
 import { User } from '../InputSource/InputSource.ts'
 
 export const handleInput = (state: SettingsState, value: string, inputSource = User): SettingsState => {
-  const { items, tabs, history, modifiedSettings, preferences } = state
+  const { items, tabs, history, modifiedSettings, preferences, height, itemHeight, scrollOffset } = state
   const filteredItems = getFilteredItems(items, tabs, value, modifiedSettings, preferences)
+  const { visibleItems, minLineY, maxLineY } = computeVisibleItems(filteredItems, height, scrollOffset, itemHeight)
+  const { scrollBarMinHeight } = state
+  const { thumbHeight, thumbTop } = computeScrollBar(height, filteredItems.length, itemHeight, scrollOffset, scrollBarMinHeight)
 
   const { newHistory, newHistoryIndex } = addToHistory(history, value)
 
@@ -13,8 +18,13 @@ export const handleInput = (state: SettingsState, value: string, inputSource = U
     ...state,
     searchValue: value,
     filteredItems,
+    visibleItems,
+    minLineY,
+    maxLineY,
     history: newHistory,
     historyIndex: newHistoryIndex,
     inputSource,
+    scrollBarThumbHeight: thumbHeight,
+    scrollBarThumbTop: thumbTop,
   }
 }

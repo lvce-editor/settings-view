7e1aa58d34fd31b3d578204dce66f6accdff4f86
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 16:37:53 +0200

    Discard changes to packages/settings-view/src/parts/GetSettingsContentDom/GetSettingsContentDom.ts

--- packages/settings-view/src/parts/GetSettingsContentDom/GetSettingsContentDom.ts
+++ packages/settings-view/src/parts/GetSettingsContentDom/GetSettingsContentDom.ts
@@ -6,39 +6,17 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getSettingsItemsDom } from '../GetSettingsItemsDom/GetSettingsItemsDom.ts'
 import * as SettingStrings from '../SettingStrings/SettingStrings.ts'
-import { getSpacerDom } from '../GetSpacerDom/GetSpacerDom.ts'
-import { getScrollBarDom } from '../GetScrollBarDom/GetScrollBarDom.ts'
 
-export const getSettingsContentDom = (
-  visibleItems: readonly DisplaySettingItem[],
-  tabs: readonly Tab[],
-  searchValue: string,
-  height: number,
-  scrollOffset: number,
-  itemHeight: number,
-  minLineY: number,
-  totalItemCount: number,
-): readonly VirtualDomNode[] => {
+export const getSettingsContentDom = (items: readonly DisplaySettingItem[], tabs: readonly Tab[], searchValue: string): readonly VirtualDomNode[] => {
   const selectedTab = tabs.find((tab) => tab.selected)
   const headerText = selectedTab ? selectedTab.label : SettingStrings.settingsContent()
-
-  const totalHeight = totalItemCount * itemHeight
-  const topSpacer = minLineY * itemHeight
-  const bottomSpacer = Math.max(0, totalHeight - topSpacer - visibleItems.length * itemHeight)
-
-  const scrollable = Math.max(0, totalHeight - height)
-  const thumbTrack = Math.max(0, height)
-  const thumbHeight = totalHeight > 0 ? Math.max(20, Math.floor((height / Math.max(totalHeight, 1)) * height)) : height
-  const thumbMaxTop = Math.max(0, thumbTrack - thumbHeight)
-  const thumbTop = scrollable > 0 ? Math.min(thumbMaxTop, Math.floor((scrollOffset / scrollable) * thumbMaxTop)) : 0
 
   return [
     {
       type: VirtualDomElements.Div,
       className: ClassNames.SettingsContent,
-      childCount: 5,
+      childCount: 2,
       onScroll: DomEventListenerFunctions.HandleScroll,
-      onWheel: DomEventListenerFunctions.HandleWheel,
     },
     {
       type: VirtualDomElements.H1,
@@ -46,9 +24,6 @@ export const getSettingsContentDom = (
       childCount: 1,
     },
     text(headerText),
-    ...getSpacerDom(topSpacer),
-    ...getSettingsItemsDom(visibleItems, searchValue),
-    ...getSpacerDom(bottomSpacer),
-    ...getScrollBarDom(thumbHeight, thumbTop),
+    ...getSettingsItemsDom(items, searchValue),
   ]
 }

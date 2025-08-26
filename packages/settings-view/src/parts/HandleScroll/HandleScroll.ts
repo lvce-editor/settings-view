be2986c431abbc3e8b5fb160e032296a5e8a3c06
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 23:59:45 +0200

    Discard changes to packages/settings-view/src/parts/HandleScroll/HandleScroll.ts

--- packages/settings-view/src/parts/HandleScroll/HandleScroll.ts
+++ packages/settings-view/src/parts/HandleScroll/HandleScroll.ts
@@ -1,15 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
-import { computeScrollBar } from '../ComputeScrollBar/ComputeScrollBar.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const handleScroll = (state: SettingsState, scrollTop: number, inputSource = InputSource.User): SettingsState => {
-  const { height, itemHeight, filteredItems, scrollBarMinHeight } = state
-  const { thumbHeight, thumbTop } = computeScrollBar(height, filteredItems.length, itemHeight, scrollTop, scrollBarMinHeight)
   return {
     ...state,
     scrollOffset: scrollTop,
     inputSource,
-    scrollBarThumbHeight: thumbHeight,
-    scrollBarThumbTop: thumbTop,
   }
 }

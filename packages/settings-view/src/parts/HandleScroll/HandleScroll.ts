a5295423431c80fd71e8c90b5f63e0b0ac9e05d1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 16:38:22 +0200

    Discard changes to packages/settings-view/src/parts/HandleScroll/HandleScroll.ts

--- packages/settings-view/src/parts/HandleScroll/HandleScroll.ts
+++ packages/settings-view/src/parts/HandleScroll/HandleScroll.ts
@@ -1,16 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
-import { computeVisibleItems } from '../ComputeVisibleItems/ComputeVisibleItems.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const handleScroll = (state: SettingsState, scrollTop: number, inputSource = InputSource.User): SettingsState => {
-  const { filteredItems, height, itemHeight } = state
-  const { visibleItems, minLineY, maxLineY } = computeVisibleItems(filteredItems, height, scrollTop, itemHeight)
   return {
     ...state,
     scrollOffset: scrollTop,
-    minLineY,
-    maxLineY,
-    visibleItems,
     inputSource,
   }
 }

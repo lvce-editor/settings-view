934aedff084f2b3e70d6ec03d45039f82e78cbb4
Le Vivilet <levivlet@pm.me>
2025-08-26 13:18:35 +0200

    update delta

--- packages/settings-view/src/parts/HandleWheel/HandleWheel.ts
+++ packages/settings-view/src/parts/HandleWheel/HandleWheel.ts
@@ -3,6 +3,7 @@ import { User } from '../InputSource/InputSource.ts'
 
 export const handleWheel = (state: SettingsState, deltaY: number, inputSource = User): SettingsState => {
   const newDeltaY = (state.deltaY || 0) + deltaY
+  console.log({ newDeltaY })
   return {
     ...state,
     deltaY: newDeltaY,

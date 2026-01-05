6d66411dbd8f38d8bbb8f2df75bd3cba702a2c9e
Le Vivilet <levivilet@pm.me>
2026-01-05 23:13:25 +0100

    fix

--- packages/settings-view/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
+++ packages/settings-view/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
@@ -1,4 +1,5 @@
-import { VirtualDomElements, VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 

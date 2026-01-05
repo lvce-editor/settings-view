eb80e103a1863578bf1a2ff3a3a73a3ff423ee07
Le Vivilet <levivilet@pm.me>
2026-01-05 22:18:53 +0100

    feature: add renderer tests

--- packages/settings-view/test/GetRenderer.test.ts
+++ packages/settings-view/test/GetRenderer.test.ts
@@ -4,6 +4,9 @@ import { getRenderer } from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
 import { renderSettingValues } from '../src/parts/RenderSettingValues/RenderSettingValues.ts'
 import { renderValue } from '../src/parts/RenderValue/RenderValue.ts'
+import { renderScrollOffset } from '../src/parts/RenderScrollOffset/RenderScrollOffset.ts'
+import { renderFocusContext } from '../src/parts/RenderFocusContext/RenderFocusContext.ts'
+import { renderFocus } from '../src/parts/RenderFocus/RenderFocus.ts'
 
 test('getRenderer returns renderItems for DiffType.RenderItems', () => {
   const renderer = getRenderer(DiffType.RenderItems)
@@ -18,6 +21,21 @@ test('getRenderer returns renderValue for DiffType.RenderValue', () => {
 test('getRenderer returns renderSettingValues for DiffType.RenderSettingValues', () => {
   const renderer = getRenderer(DiffType.RenderSettingValues)
   expect(renderer).toBe(renderSettingValues)
+})
+
+test('getRenderer returns renderScrollOffset for DiffType.RenderScrollOffset', () => {
+  const renderer = getRenderer(DiffType.RenderScrollOffset)
+  expect(renderer).toBe(renderScrollOffset)
+})
+
+test('getRenderer returns renderFocusContext for DiffType.RenderFocusContext', () => {
+  const renderer = getRenderer(DiffType.RenderFocusContext)
+  expect(renderer).toBe(renderFocusContext)
+})
+
+test('getRenderer returns renderFocus for DiffType.RenderFocus', () => {
+  const renderer = getRenderer(DiffType.RenderFocus)
+  expect(renderer).toBe(renderFocus)
 })
 
 test('getRenderer throws error for unknown diff type', () => {

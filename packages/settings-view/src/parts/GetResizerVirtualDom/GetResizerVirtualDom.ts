91b5aad74bd48b8294ef9421f02adf53d9cb9a0e
Le Vivilet <levivilet@pm.me>
2026-01-05 23:14:31 +0100

    feature: add resizer dom (#389)
    * feature: add resizer dom
    
    * fix

---
+++ packages/settings-view/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
@@ -0,0 +1,20 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const getResizerVirtualDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      childCount: 1,
+      className: ClassNames.Resizer,
+      onPointerDown: DomEventListenerFunctions.HandleResizerPointerDown,
+      type: VirtualDomElements.Div,
+    },
+    {
+      childCount: 0,
+      className: ClassNames.ResizerHighlight,
+      type: VirtualDomElements.Div,
+    },
+  ]
+}

978514c7b585bf12737190d62d210d5793544b22
Le Vivilet <levivilet@pm.me>
2026-01-06 16:20:29 +0100

    feature: add max sidebar width (#420)

--- packages/settings-view/src/parts/HandleResizerPointerMove/HandleResizerPointerMove.ts
+++ packages/settings-view/src/parts/HandleResizerPointerMove/HandleResizerPointerMove.ts
@@ -1,8 +1,9 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const handleResizerPointerMove = (state: SettingsState, eventX: number, eventY: number): SettingsState => {
-  const { sideBarMinWidth } = state
-  const newWidth = Math.max(eventX, sideBarMinWidth)
+  const { minContentWidth, sideBarMinWidth, width } = state
+  const maxSideBarWidth = width - minContentWidth
+  const newWidth = Math.max(sideBarMinWidth, Math.min(eventX, maxSideBarWidth))
   return {
     ...state,
     sideBarWidth: newWidth,

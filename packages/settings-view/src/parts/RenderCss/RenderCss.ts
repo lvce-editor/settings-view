357e917c91b3fc572ddf4fcedaa7808b909a239d
Le Vivilet <levivilet@pm.me>
2026-01-06 15:55:09 +0100

    log

--- packages/settings-view/src/parts/RenderCss/RenderCss.ts
+++ packages/settings-view/src/parts/RenderCss/RenderCss.ts
@@ -5,6 +5,5 @@ import { getCss } from '../GetCss/GetCss.ts'
 export const renderCss = (oldState: SettingsState, newState: SettingsState): any => {
   const { id, sideBarWidth } = newState
   const css = getCss(sideBarWidth)
-  console.log({ css })
   return [ViewletCommand.SetCss, id, css]
 }

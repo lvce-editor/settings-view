c4185a8f72ea2b2a1877eab0eb62e5d24edf79f6
Le Vivilet <levivilet@pm.me>
2026-01-06 17:15:07 +0100

    feature: improve buttons dom (#438)
    * feature: improve buttons dom
    
    * fix

--- packages/settings-view/src/parts/GetSettingsInputButtonsDom/GetSettingsInputButtonsDom.ts
+++ packages/settings-view/src/parts/GetSettingsInputButtonsDom/GetSettingsInputButtonsDom.ts
@@ -1,37 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import { getClearButtonClassName } from '../GetClearButtonClassName/GetClearButtonClassName.ts'
-import * as InputName from '../InputName/InputName.ts'
-import * as SettingStrings from '../SettingStrings/SettingStrings.ts'
-
-const icon: VirtualDomNode = {
-  childCount: 0,
-  className: mergeClassNames(ClassNames.MaskIcon, ClassNames.MaskIconClearAll),
-  type: VirtualDomElements.Div,
-}
+import { getClearButtonDom } from '../GetClearButtonDom/GetClearButtonDom.ts'
+import { getFilterButtonDom } from '../GetFilterButtonDom/GetFilterButtonDom.ts'
 
 export const getSettingsInputButtonsDom = (hasSearchValue: boolean): readonly VirtualDomNode[] => {
-  return [
-    {
-      ariaLabel: SettingStrings.clear(),
-      childCount: 1,
-      className: getClearButtonClassName(hasSearchValue),
-      disabled: !hasSearchValue,
-      name: InputName.Clear,
-      onClick: DomEventListenerFunctions.HandleClickClear,
-      type: VirtualDomElements.Button,
-    },
-    {
-      ariaLabel: SettingStrings.filter(),
-      childCount: 1,
-      className: ClassNames.SearchFieldButton,
-      disabled: false,
-      name: InputName.Filter,
-      onClick: DomEventListenerFunctions.HandleClickFilterButton,
-      type: VirtualDomElements.Button,
-    },
-    icon,
-  ]
+  return [...getClearButtonDom(hasSearchValue), ...getFilterButtonDom()]
 }

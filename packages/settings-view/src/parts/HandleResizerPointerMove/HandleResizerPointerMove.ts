c1ede7dc2288643afeb11c1243b9734ebb2fe979
Le Vivilet <levivilet@pm.me>
2026-01-06 10:20:24 +0100

    feature: add resizer pointer up (#394)

---
+++ packages/settings-view/src/parts/HandleResizerPointerMove/HandleResizerPointerMove.ts
@@ -0,0 +1,5 @@
+import type { SettingsState } from '../SettingsState/SettingsState.ts'
+
+export const handleResizerPointerMove = (state: SettingsState, eventX: number, eventY: number): SettingsState => {
+  return state
+}

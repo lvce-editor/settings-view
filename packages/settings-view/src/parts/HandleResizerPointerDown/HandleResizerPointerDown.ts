bd197126a57686d2c94e5aa7c488d4e078e4166e
Le Vivilet <levivilet@pm.me>
2026-01-06 10:34:49 +0100

    Discard changes to packages/settings-view/src/parts/HandleResizerPointerDown/HandleResizerPointerDown.ts

--- packages/settings-view/src/parts/HandleResizerPointerDown/HandleResizerPointerDown.ts
+++ packages/settings-view/src/parts/HandleResizerPointerDown/HandleResizerPointerDown.ts
@@ -1,6 +1,5 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const handleResizerPointerDown = (state: SettingsState, eventX: number, eventY: number): SettingsState => {
-  console.log('down', eventX, eventY)
   return state
 }

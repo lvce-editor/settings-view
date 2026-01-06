4037fe42a2e0e11180f7b48617704e0ad7d3dcb8
Le Vivilet <levivilet@pm.me>
2026-01-06 10:31:36 +0100

    feature: set initial sidebar width to zero

--- packages/settings-view/src/parts/Create/Create.ts
+++ packages/settings-view/src/parts/Create/Create.ts
@@ -27,7 +27,7 @@ export const create = (id: number, uri: string, x: number, y: number, width: num
     scrollBarThumbTop: 0,
     scrollOffset: 0,
     searchValue: '',
-    sideBarWidth: 200,
+    sideBarWidth: 0,
     tabs: [],
     uri,
     visibleItems: [],

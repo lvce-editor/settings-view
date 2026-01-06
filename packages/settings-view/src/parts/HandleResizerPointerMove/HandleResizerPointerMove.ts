d779c16b280a736d21e46681f652f035260fc5b7
Le Vivilet <levivilet@pm.me>
2026-01-06 10:38:18 +0100

    feature: implement sidebarMinWidth (#406)

--- packages/settings-view/src/parts/HandleResizerPointerMove/HandleResizerPointerMove.ts
+++ packages/settings-view/src/parts/HandleResizerPointerMove/HandleResizerPointerMove.ts
@@ -1,8 +1,10 @@
 import type { SettingsState } from '../SettingsState/SettingsState.ts'
 
 export const handleResizerPointerMove = (state: SettingsState, eventX: number, eventY: number): SettingsState => {
+  const { sideBarMinWidth } = state
+  const newWidth = Math.max(eventX, sideBarMinWidth)
   return {
     ...state,
-    sideBarWidth: eventX,
+    sideBarWidth: newWidth,
   }
 }

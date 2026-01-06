b52df88c38206a60800e7d350d2fb34732f3e437
Le Vivilet <levivilet@pm.me>
2026-01-06 17:35:42 +0100

    feature: use page object

--- packages/e2e/src/settings-search-clear.ts
+++ packages/e2e/src/settings-search-clear.ts
@@ -2,13 +2,13 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'settings.search-clear'
 
-export const test: Test = async ({ Command, expect, Locator, SettingsView }) => {
+export const test: Test = async ({ expect, Locator, SettingsView }) => {
   // arrange
   await SettingsView.show()
   await SettingsView.handleInput('font family')
 
   // act
-  await Command.execute('Settings.clear', '')
+  await SettingsView.clear('')
 
   // assert
   const input = Locator('.SettingsSearchInput')

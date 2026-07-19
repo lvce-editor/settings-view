d004799f8ba4baad87ff9eac086932830bb8f523
Le Vivilet <levivilet@pm.me>
2026-07-19 16:06:05 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import * as config from '@lvce-editor/eslint-config'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   {
     rules: {
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
@@ -14,6 +15,25 @@ export default [
       'sonarjs/assertions-in-tests': 'off',
       'sonarjs/prefer-specific-assertions': 'off',
       'unicorn/no-useless-template-literals': 'off',
+    },
+  },
+  {
+    files: [
+      'packages/settings-view/src/parts/GetSettingItemsEditor/GetSettingItemsEditor.ts',
+      'packages/settings-view/src/parts/GetSettingItemsWindow/GetSettingItemsWindow.ts',
+    ],
+    rules: {
+      'virtual-dom/no-object-attribute-values': 'off',
+    },
+  },
+  {
+    files: ['packages/settings-view/test/**/*.ts'],
+    rules: {
+      'virtual-dom/no-inline-event-handlers': 'off',
+      'virtual-dom/no-object-attribute-values': 'off',
+      'virtual-dom/prefer-constants': 'off',
+      'virtual-dom/prefer-merge-class-names': 'off',
+      'virtual-dom/valid-child-count': 'off',
     },
   },
 ]
